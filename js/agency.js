fb71d76b50af8ac888a9ddc2b9e6972b2695d1dc
frombit <frombit.main@gmail.com>
2020-10-14 16:50:18 +0800

    Site updated at 2020-10-14 08:50:18 UTC

---
+++ js/agency.js
@@ -0,0 +1,53 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function () {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
+});
+
+// change bg
+
+// console.log(document.getElementById('bg'))
+function runOnScroll () {
+    var bg = document.getElementById("bg");
+    var st = document.documentElement.scrollTop
+    // if (st >= 200) {
+    var left = (st / 10) + 25;
+    var top = (400 - st) * (1 / 4)
+    // console.log(document.documentElement.scrollTop, top);
+    // console.log(top);
+    bg.style.backgroundPosition = left + "% " + top + "%";
+    // }
+};
+window.addEventListener("scroll", runOnScroll);
